e0ff8c681d0ccfd628c0b827c0e25650749d07cc
Xinyi Huang <76785703+olehuang@users.noreply.github.com>
2025-05-29 09:54:37 +0200

    some change. next step merge main into this Branch

--- frontend/src/components/Keycloakinit.tsx
+++ frontend/src/components/Keycloakinit.tsx
@@ -3,12 +3,14 @@ import {ReactKeycloakProvider} from "@react-keycloak/web";
 import {Outlet} from "react-router-dom";
 import {useAuthHook} from "./AuthProvider";
 import {useEffect, useState} from "react";
+import {routes} from "@keycloak/keycloak-account-ui/lib/routes";
+
 
 
 const KeycloakInit = () => {
 
     const {auth,setAuth,user_id,setUserID,authLoading,setAuthLoading,setToken}=useAuthHook();
-
+    const [userInfo,setUserInfo]=useState<any>({});
 
     //listen Keycloak Certification status changes
     useEffect(() => {
@@ -19,7 +21,6 @@ const KeycloakInit = () => {
 
         };
         //after user Login fetch user Setting from MongoDB
-
     }, [user_id,auth]);
 
 
@@ -31,9 +32,16 @@ const KeycloakInit = () => {
             KeycloakClient.extractUserInfo(token)
                 .then(userInfo => {
                     if (userInfo) {
+                        setUserInfo(userInfo);
+                        saveUser(userInfo);
                         setUserID(userInfo.userId)//global
                         setAuthLoading(false);
                         setToken(token);
+                        if (userInfo.roles.includes("admin")) {
+                            uploadRole("admin");
+                        }else{
+                            uploadRole();
+                        }
                         console.log(userInfo);
                     }
                 });
@@ -42,6 +50,56 @@ const KeycloakInit = () => {
             // addAlert("Failed to obtain Keycloak token: " + error.message, "error");
         }
     };
+
+    const uploadRole =async (role="user")=>{
+         const url=`http://127.0.0.1:8000/user/update_user`;
+         try{
+             const response = await fetch(url, {
+                 method: "POST",
+                 headers: {
+                     "Content-Type": "application/json"
+                 },
+                 body: JSON.stringify(role)
+             });
+             if (!response.ok) {
+                 const errorData = await response.json();
+                 throw new Error(errorData.detail || "Upload user role faile");
+             }
+             const data = await response.json();
+             console.log("Upload user role seccessfull", data);
+         }catch (error:any){
+             console.error("Something wrong with uploading user role ：", error);
+         }
+    }
+
+    const saveUser=async (userInfo:any)=>{
+        const url=`http://127.0.0.1:8000/user/save_user`;
+        const payload={
+            user_id:userInfo.userId,
+            username:userInfo.username,
+            email:userInfo.email,
+            roles:userInfo?.roles[0],
+        }
+        try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(payload)
+        });
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.detail || "Save user faile");
+            }
+
+            const data = await response.json();
+            console.log("save user seccessfull", data);
+        }catch (error:any){
+            console.error("Something wrong with saving user ：", error);
+        }
+    }
+
     return (
     <ReactKeycloakProvider // Keycloak Provider beginn
         authClient={KeycloakClient.creatKeycloak}
